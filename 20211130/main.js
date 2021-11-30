fb596b969e74d2a3deacff3036a7063e9a7888bc
Nick Sotula <so2niko@gmail.com>
2021-11-30 19:38:04 +0200

    docs: variables

---
+++ 20211130/main.js
@@ -0,0 +1,53 @@
+// console.log('Hello');
+
+// ! Data types:
+/*
+Number
+BigInt
+String
+Boolean
+Object
+Null
+Undefined
+Symbol
+*/
+
+// ? Логические операторы
+/*
+(undefined, null, false, 0, '', 0n) == false
+все остальное == true
+
+И     
+&&
+
+ИЛИ   
+||
+
+НЕТ
+!
+*/
+
+// let a = '';
+// console.log(a || 'Нет данных');
+// console.log(!'0');
+
+// Variable declaration
+/*
+var
+
+let
+const
+
+*/
+
+// let dp221devops = 'The best group';
+// var alert = 'ne alert';
+
+// country = 'Ukraine';
+// console.log(country);
+
+// let country;
+
+// Function declaration
+// Function expression
+
